fb3ab1e2a11c2ae12c72ac90f256bf8a1a5226fe
MayQueen-BiBi <51819864+MayQueen-BiBi@users.noreply.github.com>
2026-01-08 23:21:32 +0800

    Delete 毕美乔/week12/企业智能助手/app/session directory

--- 毕美乔/week12/企业智能助手/app/session/__init__.py
+++
@@ -1,9 +0,0 @@
-from .state import SessionState
-from .manager import (get_or_create_session,
-                      get_or_create_agent_runtime,
-                      destroy_agent_runtime)
-
-__all__ = ["SessionState",
-           "get_or_create_session",
-           "get_or_create_agent_runtime",
-           "destroy_agent_runtime"]
