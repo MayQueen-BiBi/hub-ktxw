06600ac87a402094a0f075077b007bfbeeb75b8f
MayQueen-BiBi <51819864+MayQueen-BiBi@users.noreply.github.com>
2026-01-08 12:34:38 +0800

    Change tool MCP prefix to 'tools' in mcp_server_main.py

--- 毕美乔/week12/企业智能助手/app/mcp_server/mcp_server_main.py
+++ 毕美乔/week12/企业智能助手/app/mcp_server/mcp_server_main.py
@@ -1,10 +1,10 @@
 import asyncio
 from fastmcp import FastMCP, Client
 
-from news import mcp as news_mcp
-from saying import mcp as saying_mcp
-from tool import mcp as tool_mcp
-from sentiment import mcp as sentiment_mcp
+from .news import mcp as news_mcp
+from .saying import mcp as saying_mcp
+from .tool import mcp as tool_mcp
+from .sentiment import mcp as sentiment_mcp
 
 
 # 创建 MCP Server
@@ -17,7 +17,7 @@ mcp = FastMCP(
 async def setup():
     await mcp.import_server(news_mcp, prefix="news")
     await mcp.import_server(saying_mcp, prefix="saying")
-    await mcp.import_server(tool_mcp, prefix="tool")
+    await mcp.import_server(tool_mcp, prefix="tools")
     await mcp.import_server(sentiment_mcp, prefix="sentiment")
 
 
